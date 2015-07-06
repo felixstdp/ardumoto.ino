205f7a345fba711d5a6767e6b8734f6a09ea5c48
fstdp <fstdp@hotmail.com>
2015-07-06 18:09:17 +0200

    Update Ardumoto.cpp

--- Ardumoto.cpp
+++ Ardumoto.cpp
@@ -1,50 +1,57 @@
 #include <Arduino.h>
 #include "Ardumoto.h"
 
+// define los pins de la shield Ardumoto de control de motores
+
+#define dirL 13 // pin de dirección para la rueda izquierda (B)
+#define velL 11 // pin de velocidad para la rueda izquierda
+#define dirR 12 // pin de dirección para la rueda derecha (A)
+#define velR 3 // pin de velocidad para la rueda derecha
+
 Ardumoto::Ardumoto(){
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
+  pinMode(dirL,OUTPUT);
+  pinMode(velL,OUTPUT);
+  pinMode(dirR,OUTPUT);
+  pinMode(velR,OUTPUT);
 }
 
 void Ardumoto::forward(int l, int r, int t)
 {
-  analogWrite(10,l);
-  analogWrite(11,r);
-  digitalWrite(12,HIGH);
-  digitalWrite(13,HIGH);
+  analogWrite(velL,l);
+  analogWrite(velR,r);
+  digitalWrite(dirL,HIGH);
+  digitalWrite(dirR,HIGH);
   delay(t);
 }
 
 void Ardumoto::reverse(int l, int r, int t)
 {
-  analogWrite(10,l);
-  analogWrite(11,r);
-  digitalWrite(12,LOW);
-  digitalWrite(13,LOW);
+  analogWrite(velL,l);
+  analogWrite(velR,r);
+  digitalWrite(dirL,LOW);
+  digitalWrite(dirR,LOW);
   delay(t);
 }
 
 void Ardumoto::brake(int t)
 {
-  analogWrite(10,0);
-  analogWrite(11,0);
+  analogWrite(velL,0);
+  analogWrite(velR,0);
   delay(t);
 }
 
 void Ardumoto::rotate(int t)
 {
-  analogWrite(10,100);
-  analogWrite(11,100);
+  analogWrite(velL,100);
+  analogWrite(velR,100);
   if (t>0) {
-    digitalWrite(12,LOW);
-    digitalWrite(13,HIGH);
+    digitalWrite(dirR,LOW);
+    digitalWrite(dirL,HIGH);
   }
   else
   {
-    digitalWrite(12,HIGH);
-    digitalWrite(13,LOW);
+    digitalWrite(dirR,HIGH);
+    digitalWrite(dirL,LOW);
   }
   delay(abs(t));
 }
